5bffb41c77557301cdd01eda382ca0e9cd3b4f1f
Valerie Wilson <valerie.n.wilson@gmail.com>
2016-12-16 15:40:15 -0800

    Got weather API working

--- window.js
+++ window.js
@@ -3,13 +3,49 @@
 $(document).ready(function() {
 
   $.ajax({
-    url: "http://api.wunderground.com/api/ef5a156e62f050d2/conditions/q/OH/Columbus.json",
+    url: "http://api.wunderground.com/api/ef5a156e62f050d2/conditions/q/WA/Seattle.json",
+    dataType: "json",
+    success: function(url) {
+      var temp_f = url.current_observation.temp_f;
+      $("#seattle-weather").html(temp_f + "ºF");
+    }
+  });
+
+  $.ajax({
+    url: "http://api.wunderground.com/api/ef5a156e62f050d2/conditions/q/IL/Chicago.json",
+    dataType: "json",
+    success: function(url) {
+      var temp_f = url.current_observation.temp_f;
+      $("#chicago-weather").html(temp_f + "ºF");
+    }
+  });
+
+  $.ajax({
+    url: "http://api.wunderground.com/api/ef5a156e62f050d2/conditions/q/NY/New_York.json",
+    dataType: "json",
+    success: function(url) {
+      var temp_f = url.current_observation.temp_f;
+      $("#nyc-weather").html(temp_f + "ºF");
+    }
+  });
+
+  $.ajax({
+    url: "http://api.wunderground.com/api/ef5a156e62f050d2/conditions/q/HI/Maui.json",
+    dataType: "json",
+    success: function(url) {
+      var temp_f = url.current_observation.temp_f;
+      $("#maui-weather").html(temp_f + "ºF");
+    }
+  });
+
+  $.ajax({
+    url: "http://api.wunderground.com/api/ef5a156e62f050d2/conditions/q/OR/Portland.json",
     dataType: "json",
     success: function(url) {
       console.log(url);
       var location = 'Columbus';
       var temp_f = url.current_observation.temp_f;
-      $(".conditions").html("Current temperature in " + location + " is: " + temp_f + "ºF");
+      $("#portland-weather").html(temp_f + "ºF");
     }
   });
 });
