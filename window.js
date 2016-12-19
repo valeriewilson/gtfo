6ba15e3f7fa1cae462651b58949f6d4b989afe8d
Valerie Wilson <valerie.n.wilson@gmail.com>
2016-12-18 16:48:34 -0800

    Added actual forecasted weather instead of current temps, formatted as needed

--- window.js
+++ window.js
@@ -3,57 +3,67 @@
 $(document).ready(function() {
 
   $.ajax({
-    url: "http://api.wunderground.com/api/ef5a156e62f050d2/conditions/q/WA/Seattle.json",
+    url: "http://api.wunderground.com/api/ef5a156e62f050d2/forecast10day/q/WA/Seattle.json",
     dataType: "json",
     success: function(url) {
-      var temp_f = url.current_observation.temp_f;
-      var conditions = url.current_observation.icon_url;
-      $("#seattle-weather").html(temp_f + "ºF");
-      $("#seattle-weather-img").attr("src", conditions);
+      for (i = 1; i < 9; i++) {
+        base_url = url.forecast.simpleforecast.forecastday[i];
+        $("#seattle-day-" + i).html(base_url.date.weekday + ", " + base_url.date.month + "/" + base_url.date.day);
+        $("#seattle-icon-" + i).attr("src", base_url.icon_url);
+        $("#seattle-temp-" + i).html(base_url.low.fahrenheit + "ºF - " + base_url.high.fahrenheit + "ºF");
+      }
     }
   });
 
   $.ajax({
-    url: "http://api.wunderground.com/api/ef5a156e62f050d2/conditions/q/IL/Chicago.json",
+    url: "http://api.wunderground.com/api/ef5a156e62f050d2/forecast10day/q/IL/Chicago.json",
     dataType: "json",
     success: function(url) {
-      var temp_f = url.current_observation.temp_f;
-      var conditions = url.current_observation.icon_url;
-      $("#chicago-weather").html(temp_f + "ºF");
-      $("#chicago-weather-img").attr("src", conditions);
+      for (i = 1; i < 9; i++) {
+        base_url = url.forecast.simpleforecast.forecastday[i];
+        $("#chicago-day-" + i).html(base_url.date.weekday + ", " + base_url.date.month + "/" + base_url.date.day);
+        $("#chicago-icon-" + i).attr("src", base_url.icon_url);
+        $("#chicago-temp-" + i).html(base_url.low.fahrenheit + "ºF - " + base_url.high.fahrenheit + "ºF");
+      }
     }
   });
 
   $.ajax({
-    url: "http://api.wunderground.com/api/ef5a156e62f050d2/conditions/q/NY/New_York.json",
+    url: "http://api.wunderground.com/api/ef5a156e62f050d2/forecast10day/q/NY/New_York.json",
     dataType: "json",
     success: function(url) {
-      var temp_f = url.current_observation.temp_f;
-      var conditions = url.current_observation.icon_url;
-      $("#nyc-weather").html(temp_f + "ºF");
-      $("#nyc-weather-img").attr("src", conditions);
+      for (i = 1; i < 9; i++) {
+        base_url = url.forecast.simpleforecast.forecastday[i];
+        $("#nyc-day-" + i).html(base_url.date.weekday + ", " + base_url.date.month + "/" + base_url.date.day);
+        $("#nyc-icon-" + i).attr("src", base_url.icon_url);
+        $("#nyc-temp-" + i).html(base_url.low.fahrenheit + "ºF - " + base_url.high.fahrenheit + "ºF");
+      }
     }
   });
 
   $.ajax({
-    url: "http://api.wunderground.com/api/ef5a156e62f050d2/conditions/q/HI/Maui.json",
+    url: "http://api.wunderground.com/api/ef5a156e62f050d2/forecast10day/q/HI/Maui.json",
     dataType: "json",
     success: function(url) {
-      var temp_f = url.current_observation.temp_f;
-      var conditions = url.current_observation.icon_url;
-      $("#maui-weather").html(temp_f + "ºF");
-      $("#maui-weather-img").attr("src", conditions);
+      for (i = 1; i < 9; i++) {
+        base_url = url.forecast.simpleforecast.forecastday[i];
+        $("#maui-day-" + i).html(base_url.date.weekday + ", " + base_url.date.month + "/" + base_url.date.day);
+        $("#maui-icon-" + i).attr("src", base_url.icon_url);
+        $("#maui-temp-" + i).html(base_url.low.fahrenheit + "ºF - " + base_url.high.fahrenheit + "ºF");
+      }
     }
   });
 
   $.ajax({
-    url: "http://api.wunderground.com/api/ef5a156e62f050d2/conditions/q/OR/Portland.json",
+    url: "http://api.wunderground.com/api/ef5a156e62f050d2/forecast10day/q/OR/Portland.json",
     dataType: "json",
     success: function(url) {
-      var temp_f = url.current_observation.temp_f;
-      var conditions = url.current_observation.icon_url;
-      $("#portland-weather").html(temp_f + "ºF");
-      $("#portland-weather-img").attr("src", conditions);
+      for (i = 1; i < 9; i++) {
+        base_url = url.forecast.simpleforecast.forecastday[i];
+        $("#portland-day-" + i).html(base_url.date.weekday + ", " + base_url.date.month + "/" + base_url.date.day);
+        $("#portland-icon-" + i).attr("src", base_url.icon_url);
+        $("#portland-temp-" + i).html(base_url.low.fahrenheit + "ºF - " + base_url.high.fahrenheit + "ºF");
+      }
     }
   });
 });
